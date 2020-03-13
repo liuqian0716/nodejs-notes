c76ea1097af76630989b5e078f2af7cd97a04407
v_qainiliu <1215381389@qq.com>
2020-03-13 11:54:57 +0800

    writeStream

---
+++ 5.stream/3.wariteStream.js
@@ -0,0 +1,46 @@
+// var fs = require('fs');
+
+// var rs = fs.createReadStream('line.txt');
+// var ws = fs.createWriteStream('write.txt');
+
+// ws.on('open', function(){
+//     console.log('写入文件打开');
+// })
+// rs.on('data', function(data){
+//      console.log(data);
+//      ws.write(data)
+// })
+// rs.on('end', function(){
+//     console.log('读取完毕');
+//     ws.end('写入完毕', function(data){
+//         console.log(ws.bytesWritten);
+//     })
+// })
+
+
+// 使用drain
+// var fs = require('fs');
+
+// var ws = fs.createWriteStream('test.txt', {
+//     highWatermark: 1024
+// })
+// writeMillon(ws,'data', 'utf8', function(){})
+// function writeMillon(writer, data, encoding, callback){
+//     var i = 1000000;
+//     write()
+//     function write(){
+//         var ok = true;
+//         do{
+//             i -= 1;
+//             if(i === 0){
+//                 writer.write(data, encoding, callback)
+//             }else{
+//                 ok = writer.write(data, encoding, callback)
+//                 console.log(ok);
+//             }
+//         }while(i>0 && ok)
+//         if(i > 0){
+//             writer.once('drain', write)
+//         }
+//     }
+// }
